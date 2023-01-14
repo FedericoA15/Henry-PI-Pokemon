d923f38c82dcc63995e2650dab0f38fa3af78cb7
unknown <federicoalmeida15@gmail.com>
2023-01-14 02:56:51 -0300

    Based date v.1

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
+    Id: {
+      type: DataTypes.INTEGER,
+      primarykey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
+    },
+    life:{
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
   });
 };
---
+++ api/src/models/Type.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Type', {
+    Id:{
+        type:DataTypes.INTEGER,
+        allowNull: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+  });
+};
